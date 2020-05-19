17a9a3c3c8da5485b6ca0c03c863e50f69e8b616
Andrei Kondratyev <a.kondratyev@jetmail.cc>
2020-05-19 13:39:10 +0300

    v.0.0.2
    find by tag, class and id

--- deparser.py
+++ deparser.py
@@ -1,60 +1,92 @@
-from html.parser import HTMLParser
-from lxml import html
+# def test(*args, id_="", class_="", attrs_=""):
+def test(tag, id_="", class_="", attrs_=""):
+    print(tag)
+    print(class_)
+    print(id_)
+    print(attrs_)
 
-file = open('documentation.html', 'r').read()
 
-tree = html.fromstring(file)
-allH2 = tree.xpath('//div[@class="related"]')
-print(allH2)
+exec_params = {"class": "class_",
+               "id": "id_",
+               "attr": "attrs_"}
 
+input_mass = map(lambda elem: str(elem).lstrip(),
+                 "tag=div; id=my-beautifull-pony; class=once upon a time; attr=single".split(";"))
+print(input_mass)
+
+tag = None
+tags_params = {}
+
+for elem in input_mass:
+    print("element "+elem)
+
+    key, values = elem.split("=")
+
+    key = exec_params[key] if key in exec_params else key
+
+    tags_params[key] = values
+
+    print(tags_params)
+
+test(tags_params.pop("tag"), **tags_params)
+
+# from html.parser import HTMLParser
+# from lxml import html
+
+# file = open('documentation.html', 'r').read()
+
+# tree = html.fromstring(file)
+# allH2 = tree.xpath('//div[@class="related"]')
+# print(allH2)
+
+
+# # class MyHTMLParser(HTMLParser):
+# #     def handle_starttag(self, tag, attrs):
+# #         print("Encountered a start tag:", tag)
+
+# #     def handle_endtag(self, tag):
+# #         print("Encountered an end tag :", tag)
+
+# #     def handle_data(self, data):
+# #         print("Encountered some data  :", data)
+
+# # parser = MyHTMLParser()
+# # parser.feed(file)
 
 # class MyHTMLParser(HTMLParser):
 #     def handle_starttag(self, tag, attrs):
-#         print("Encountered a start tag:", tag)
+#         global string
+#         string += "Start tag: {tag} \n"
+#         for attr in attrs:
+#             string += "     attr: {attr} \n"
+
+#         return string
 
 #     def handle_endtag(self, tag):
-#         print("Encountered an end tag :", tag)
+#         global string
+#         string += "End tag  : {tag} \n"
+
+#         return string
 
 #     def handle_data(self, data):
-#         print("Encountered some data  :", data)
+#         global string
+#         string += "Data     : {data} \n"
 
-# parser = MyHTMLParser()
-# parser.feed(file)
+#         return string
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        global string
-        string += "Start tag: {tag} \n"
-        for attr in attrs:
-            string += "     attr: {attr} \n"
+#     def handle_comment(self, data):
+#         print("Comment  :", data)
 
-        return string
+#     def handle_entityref(self, name):
+#         c = chr(name2codepoint[name])
+#         print("Named ent:", c)
 
-    def handle_endtag(self, tag):
-        global string
-        string += "End tag  : {tag} \n"
+#     def handle_charref(self, name):
+#         if name.startswith('x'):
+#             c = chr(int(name[1:], 16))
+#         else:
+#             c = chr(int(name))
+#         print("Num ent  :", c)
 
-        return string
-
-    def handle_data(self, data):
-        global string
-        string += "Data     : {data} \n"
-
-        return string
-
-    def handle_comment(self, data):
-        print("Comment  :", data)
-
-    def handle_entityref(self, name):
-        c = chr(name2codepoint[name])
-        print("Named ent:", c)
-
-    def handle_charref(self, name):
-        if name.startswith('x'):
-            c = chr(int(name[1:], 16))
-        else:
-            c = chr(int(name))
-        print("Num ent  :", c)
-
-    def handle_decl(self, data):
-        print("Decl     :", data)
+#     def handle_decl(self, data):
+#         print("Decl     :", data)
--- interface.py
+++ interface.py
@@ -25,32 +25,49 @@ def FindInFile():
     file_content = open(fn, 'rt').read()
     soup = BeautifulSoup(file_content, 'html.parser')
     
+    # v 0.0.1b
     # tag, class_name = start_tag.get().split(',')
     # result = soup.find(tag, class_=class_name).prettify()
     # text_box.delete('1.0', 'end')
     # text_box.insert('1.0', result)
-    search_tag = start_tag.get()
 
-    if search_tag:
-        try:
-            tag, class_name = search_tag.split(',')
-            result = soup.find(tag, class_=class_name).prettify()
-            text_box.delete('1.0', 'end')
-            text_box.insert('1.0', result)
-        except ValueError:
-            print("MOLOKO")
-        except AttributeError:
-            print("KOFE")
-        try:
-            tag = search_tag.split(',')
-            print(tag)
-            print("KEFIR")
-            result = soup.find(tag).prettify()
-        except:
-            print("KAKAO")
-    else:
-        text_box.delete('1.0', 'end')
-        text_box.insert('1.0', 'You do not enter "tag"')
+    # v 0.0.1
+    # if search_tag:
+    #     try:
+    #         tag, class_name = search_tag.split(',')
+    #         result = soup.find(tag, class_=class_name).prettify()
+    #         text_box.delete('1.0', 'end')
+    #         text_box.insert('1.0', result)
+    #     except ValueError:
+    #         print("MOLOKO")
+    #     except AttributeError:
+    #         print("KOFE")
+    #     try:
+    #         tag = search_tag.split(',')
+    #         print(tag)
+    #         print("KEFIR")
+    #         result = soup.find(tag).prettify()
+    #     except:
+    #         print("KAKAO")
+    # else:
+    #     text_box.delete('1.0', 'end')
+    #     text_box.insert('1.0', 'You do not enter "tag"')
+
+    # v 0.0.2
+    search_tag = start_tag.get()
+    input_mass = map(lambda elem: str(elem).lstrip(), search_tag.split(";"))
+
+    exec_params = {"class": "class_"}
+    tags_params = {}    
+
+    for elem in input_mass:
+        key, values = elem.split("=")
+        key = exec_params[key] if key in exec_params else key
+        tags_params[key] = values
+
+    result = soup.find(tags_params.pop("tag"), **tags_params).prettify()
+    text_box.delete('1.0', 'end')
+    text_box.insert('1.0', result)
 
 
 def StartRemoving():
@@ -62,11 +79,31 @@ def StartRemoving():
             if filename.endswith('.html'):
                 file_content = open(filename, 'rt').read()
                 soup = BeautifulSoup(file_content, 'html.parser')
-                tag, class_name = start_tag.get().split(',')   
-                if not class_name:
-                    result = soup.find(tag).prettify()
-                else:
-                    result = soup.find(tag, class_=class_name).prettify()
+
+                # v 0.0.1
+                # tag, class_name = start_tag.get().split(',')   
+                # if not class_name:
+                #     result = soup.find(tag).prettify()
+                # else:
+                #     result = soup.find(tag, class_=class_name).prettify()
+                # filename = "new_"+filename
+                # open(filename, 'wt').write(result)
+                # text_box.delete('1.0', 'end')
+                # text_box.insert('1.0', "Done")
+
+                # v 0.0.2
+                search_tag = start_tag.get()
+                input_mass = map(lambda elem: str(elem).lstrip(), search_tag.split(";"))
+
+                exec_params = {"class": "class_"}
+                tags_params = {}    
+
+                for elem in input_mass:
+                    key, values = elem.split("=")
+                    key = exec_params[key] if key in exec_params else key
+                    tags_params[key] = values
+
+                result = soup.find(tags_params.pop("tag"), **tags_params).prettify()
                 filename = "new_"+filename
                 open(filename, 'wt').write(result)
                 text_box.delete('1.0', 'end')
@@ -94,47 +131,49 @@ start_tag_entry = Entry(app, textvariable=start_tag, width=40)
 start_tag_entry.grid(row=1, column=1, columnspan=2)
 
 # description label
-label = Label(app, text='Intag input you write tag first than class after comma, without space')
-label.grid(row=2, column=0, columnspan=2, sticky=W)
-label2 = Label(app, text='example: "div,related", equal "div class="related""')
+label1 = Label(app, text='In tag input you write tag=div first, after semicolon')
+label1.grid(row=2, column=0, columnspan=2, sticky=W)
+label2 = Label(app, text='you can type class or id that separating them semicolon')
 label2.grid(row=3, column=0, columnspan=2, sticky=W)
+label3 = Label(app, text='example: tag=div; class=related; id="first" equal "div class="related" id="first"')
+label3.grid(row=4, column=0, columnspan=2, sticky=W)
 
 # text after serching
 scroll_for_tb = Scrollbar(app)
-scroll_for_tb.grid(row=4, column=3, sticky=N+S+W)
+scroll_for_tb.grid(row=5, column=3, sticky=N+S+W)
 
 text_box = Text(app, height=20, border=1)
-text_box.grid(row=4, column=0, columnspan=3, padx=10, pady=10)
+text_box.grid(row=5, column=0, columnspan=3, padx=10, pady=10)
 text_box.configure(yscrollcommand=scroll_for_tb.set)
 
 scroll_for_tb.configure(command=text_box.yview)
 
 # open file buttin
 open_buttom = Button(app, text="Open File", width=12, command=OpenFile)
-open_buttom.grid(row=5, column=0)
+open_buttom.grid(row=6, column=0)
 
 # start button
 start_button = Button(app, text="Start", width=12, command=FindInFile)
-start_button.grid(row=5, column=1)
+start_button.grid(row=6, column=1)
 
 # save one file button
 save_button = Button(app, text="Save", width=12, command=SaveFile)
-save_button.grid(row=5, column=2)
+save_button.grid(row=6, column=2)
 
 # button for find child of tag in files
-child_button = Button(app, text="Children tag", width=12)
-child_button.grid(row=6, column=0)
+# child_button = Button(app, text="Children tag", width=12)
+# child_button.grid(row=7, column=0)
 
 # button for find parent of tag in files
 parent_button = Button(app, text="All files", width=12, command=StartRemoving)
-parent_button.grid(row=6, column=1)
+parent_button.grid(row=7, column=1)
 
 # Quit button
 quit_button = Button(app, text="Quit", width=12, command=Quit)
-quit_button.grid(row=6, column=2)
+quit_button.grid(row=7, column=2)
 
 app.title('Deparser')
-app.geometry('630x470')
+app.geometry('640x500')
 
 app.mainloop()
 
